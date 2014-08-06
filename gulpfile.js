d9c21860838f32f97b602e2a46eb41b54db6aa7b
Cody Rushing <codyrushing@gmail.com>
2014-08-05 23:59:46 -0500

    browserify and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 var gulp = require("gulp"),
-	shell = require("gulp-shell"),
 	gulpif = require("gulp-if"),
 	nodemon = require("gulp-nodemon"),
+	browserify = require("gulp-browserify"),
 	paths = {
 
 	},
 	nodeArgs = process.argv.filter(function(arg){
 		return arg.indexOf("--") === 0;
-	}),
-	server = function(){
-		nodemon({	
-			script: "app.js",
-			ext: "js",
-			nodeArgs: nodeArgs
-		});
-		if(nodeArgs.length){
-			require("child_process").spawn("node-inspector");
-		}			
-	};
+	});
 
-gulp.task("dev", [], function(){
-	server();
+gulp.task("scripts", function(){
+	gulp.src("public/js/app.js")
+		.pipe(browserify())
+		.pipe( gulp.dest("public/js/dist") );
 });
+
+gulp.task("server", function(){
+	nodemon({	
+		script: "app.js",
+		ext: "js",
+		nodeArgs: nodeArgs
+	});
+	if(nodeArgs.length){
+		require("child_process").spawn("node-inspector");
+	}			
+});
+
+gulp.task("dev", ["server"]);
