748ebb38f119d69b38960a525c24c7b5277f4457
Cody Rushing <codyrushing@gmail.com>
2014-08-31 01:45:59 -0500

    gulp improvements, adding compass, fixing jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 var gulp = require("gulp"),
 	gulpif = require("gulp-if"),
 	nodemon = require("gulp-nodemon"),
+	plumber = require("gulp-plumber"),
+	compass = require("gulp-compass"),
 	browserify = require("gulp-browserify"),
 	jshint = require("gulp-jshint"),
+	path = require("path");
 	paths = {
 		src: {
-			js: "public/src/js/"
+			js: "public/src/js/",
+			sass: "public/sass"
 		},
 		dest: {
-			js: "public/js/"
+			js: "public/js/",
+			css: "public/css"
 		}
 	},
 	nodeArgs = process.argv.filter(function(arg){
@@ -17,10 +22,12 @@ var gulp = require("gulp"),
 
 gulp.task("scripts", function(){
 	gulp.src(paths.src.js + "**/*.js")
+		.pipe(plumber())
 		.pipe(jshint())
-		.pipe(jshint.reporter("default"));
+		.pipe(jshint.reporter("jshint-stylish"));
 
 	gulp.src(paths.src.js + "app.js")
+		.pipe(plumber())
 		.pipe(browserify())
 		.pipe( gulp.dest( paths.dest.js ) );
 });
@@ -37,8 +44,18 @@ gulp.task("server", function(){
 	}			
 });
 
-gulp.task("watch", function(){
-	gulp.watch([paths.src.js + "**/*.js"], ["scripts"]);
+gulp.task("compass", function(){
+	gulp.src("public/sass/*.scss")
+		.pipe(compass({
+			project: path.join(__dirname, "public"),
+			config_file: "./config.rb"
+		}))
+		.pipe(gulp.dest( paths.dest.css ))
 });
 
-gulp.task("dev", ["server", "scripts", "watch"]);
+gulp.task("watch", function(){
+	gulp.watch([paths.src.js + "**/*.js"], ["scripts"]);
+	gulp.watch([paths.src.sass + "**/*.scss"], ["compass"])
+});
+
+gulp.task("dev", ["server", "scripts", "compass", "watch"]);
